fac677bb44ed678eb2e88c3658dbe555967f3905
Saveliy21 <vildanov.s@bk.ru>
2023-04-03 19:31:21 +0500

    task-3.4 final

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -6,26 +6,9 @@ public class Book {
     String author;
     Integer yearOfPublication;
 
-    @Override
-    public boolean equals(Object o){
-        if(o == this){
-            return true;
-        }
-        if (! (o instanceof Book)){
-            return false;
-        }
-        Book book = (Book) o;
-        return book.yearOfPublication.equals(yearOfPublication) &&
-                book.title.equals(title) &&
-                book.author.equals(author);
+
+    public Integer getYear(){
+        return this.yearOfPublication;
     }
 
-    @Override
-    public int hashCode(){
-        int hash = 7;
-        hash = 31 * hash + yearOfPublication;
-        hash = 31 * hash + (title == null ? 0 : title.hashCode());
-        hash = 31 * hash + (author == null ? 0 : author.hashCode());
-        return hash;
-    }
 }
--- src/main/java/org/example/library.java
+++ src/main/java/org/example/library.java
@@ -5,13 +5,19 @@ import java.util.*;
 public class library {
 
 
-    public Map<Book, Integer> library = new LinkedHashMap<>();
-
-    public Set<Book> lib = new LinkedHashSet<>();
-
+    /*
+       Я очень долго мучался с выбором более сложной коллекции, но у меня
+       возникали большие трусдности с реализацией методов поиска, поэтому я решил
+       обратиться к листам, выбор пал на LinkedList т.к. к элементам по индексу мы почти не обращаемся
+       а вставка/ удаление будет происходить чаще и быстрее засчёт двусвязного списка.
+       сами же операции будут проходить за O(n) т.к. в каждом методе мы итерируемся по коллекции до n-го элемента.
+       последний метод getFromIntervalOfPublic тоже будет за O(n) т.к. я сделал сортировку по годам.
+     */
+    public List<Book> library = new LinkedList<>();
 
     public void addBook(Book book){
-        library.put(book, book.hashCode());
+        library.add(book);
+        library.sort(Comparator.comparing(Book::getYear));
     }
 
     public void removeBook(Book book){
@@ -19,18 +25,45 @@ public class library {
     }
 
     public List<Book> getFromTitle(String title){
-
+        List<Book> result =  new LinkedList<>();
+        for(Book book : library){
+            if(Objects.equals(book.title, title)){
+                result.add(book);
+            }
+        }
+        return result;
     }
 
-    public Book getFromAuthor(String author){
-
+    public List<Book> getFromAuthor(String author){
+        List<Book> result =  new LinkedList<>();
+        for(Book book : library){
+            if(Objects.equals(book.author, author)){
+                result.add(book);
+            }
+        }
+        return result;
     }
 
-    public Book getFromYear(Integer year){
-
+    public List<Book> getFromYear(Integer year){
+        List<Book> result =  new LinkedList<>();
+        for(Book book : library){
+            if(Objects.equals(book.getYear(), year)){
+                result.add(book);
+            }
+        }
+        return result;
     }
 
-    public Book getFromIntervalOfPublic(){
-
+    // выдаст все книги [start;end], где start - начальный год, end - конечный
+    public List<Book> getFromIntervalOfPublic(int start, int end){
+        List<Book> result = new LinkedList<>();
+        for(Book book : library){
+            if(book.getYear() >= start){
+                result.add(book);
+            }
+            if(book.getYear() > end)
+                break;
+        }
+        return result;
     }
 }
