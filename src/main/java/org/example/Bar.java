2a471d15daf40c86426c3394b38539066b1e97cf
Saveliy21 <vildanov.s@bk.ru>
2023-04-03 17:02:58 +0500

    task-3.2 final

--- src/main/java/org/example/Bar.java
+++ src/main/java/org/example/Bar.java
@@ -3,22 +3,18 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Bar {
+public class Bar{
 
     /* Я выбрал именно HashMap, потому что добавление и получение элементов у нас работают за O(1),
-     при условии, что хэш функция определена нормально.
-     Я определил её в классе Ticket как сам номер билета, переведённый в 10-тичную запись.
-     Не уверен насчёт корректности самой map и класса Ticket, но у меня были мысли такие, что
-     в классе Ticket у нас может быть много различных полей, например: время и дата покупки, Id кассира и многое другое.
-     А поскольку нас интересуют только сам номер билета и доп. продукты к нему на баре,
-     мы заводим map только с этими полями.
+     при условии, что хэш функция определена нормально, если я прав, то тут мы сраваниваем строки, а для String метод
+     equals() уже переопределён, а значит и hashCode() тоже (по идее).
     */
     public Map<String,String> tickets = new HashMap<>();
 
-    public void addVisitors(String hexadecimalNumber, String additions){
-        Ticket ticket = new Ticket(hexadecimalNumber, additions);
-        tickets.put(ticket.hexadecimalNumber, ticket.additions);
+    public void addVisitor(String hexadecimalNumber, String additions){
+        tickets.put(hexadecimalNumber, additions);
     }
+
     public String getProduct(String number){
         return tickets.get(number);
     }
