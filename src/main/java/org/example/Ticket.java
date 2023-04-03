7437d428f3c561234bcaeadd1b04469cf0c77fda
Saveliy21 <vildanov.s@bk.ru>
2023-04-03 16:19:22 +0500

    task-3.2 update

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -12,6 +12,18 @@ public class Ticket {
     }
 
     @Override
+    public boolean equals(Object o){
+        if(o == this){
+            return true;
+        }
+        if(!(o instanceof Ticket)){
+            return  false;
+        }
+        Ticket ticket = (Ticket)o;
+        return ticket.hexadecimalNumber.equals(hexadecimalNumber) && ticket.additions.equals(additions);
+    }
+
+    @Override
     public int hashCode(){
         return Integer.parseInt(hexadecimalNumber, 16);
     }
